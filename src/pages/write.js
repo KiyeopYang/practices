15123b1334f81585a75c77e977f7163954f9ce62
kiyeopyang <kiyeopyang@gmail.com>
2020-03-09 16:29:12 +0900

    test init

--- src/pages/write.js
+++ src/pages/write.js
@@ -1,19 +1,42 @@
-import React, { useState } from 'react';
-import Layout from 'components/Layout';
-import Link from 'next/link';
-import List from 'components/List';
-import PostWriter from 'containers/PostWriter';
+import React, { useState, useEffect } from "react";
+import Layout from "components/Layout";
+import Link from "next/link";
+import List from "components/List";
+import PostWriter from "containers/PostWriter";
+import { useRouter } from "next/router";
 
 export default () => {
-	const [items, setItems] = useState([]);
-	return (
-		<Layout>
-			<h4>페이지 2</h4>
-			<Link href="/">
-				<a>페이지 1</a>
-			</Link>
-			<PostWriter onAdd={item => setItems([...items, item])} />
-			<List items={items} />
-		</Layout>
-	);
+  const [items, setItems] = useState([]);
+  const Router = useRouter();
+  useEffect(() => {
+    console.log(Router.query);
+  }, [Router.query]);
+  return (
+    <Layout>
+      <h4>페이지 2</h4>
+      <Link href="/">
+        <a>페이지 1</a>
+      </Link>
+      <PostWriter onAdd={item => setItems([...items, item])} />
+      <List items={items} />
+      <div>
+        <button
+          onClick={() =>
+            Router.push("/write?selected=A", "/write?selected=A", true)
+          }
+        >
+          A
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() =>
+            Router.push("/write?selected=B", "/write?selected=B", true)
+          }
+        >
+          B
+        </button>
+      </div>
+    </Layout>
+  );
 };
